73ab79968693aa78fc2efeacb8bc992102119d27
NN <NN@PC12>
2019-04-21 12:01:20 +0700

    20190421

--- 20190413/src/com/b3/Program.java
+++ 20190413/src/com/b3/Program.java
@@ -1,7 +1,10 @@
 package com.b3;
 
+import java.util.Scanner;
+
 public class Program {
 
+	static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
 		basic();
 
@@ -14,4 +17,18 @@ public class Program {
 		
 	}
 
+	private static void advance() {
+		Student[] dssv = new Student[5];
+
+		for (Student stu : dssv) {
+			System.out.print("Nhap ho ten sv:");
+			String hovaten = scanner.nextLine();
+			System.out.println("Nhap diem:");
+			int diem = scanner.nextInt();
+			stu = new Student(hovaten, diem);
+		}
+		for (Student stu : dssv) {
+			System.out.println(stu.fullName + stu.mark +" diem. Xep loai:"+ stu.getGrade());
+		}
+	}
 }
